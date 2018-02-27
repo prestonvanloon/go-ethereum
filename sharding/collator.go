2cd821806e1e149174d4618c182bfc086808af04
Raul Jordan <rauljordan@college.harvard.edu>
2018-02-26 22:38:27 -0600

    refactor account, and unlock account when client is created

--- sharding/collator.go
+++ sharding/collator.go
@@ -9,6 +9,15 @@ import (
 	"math/big"
 )
 
+<<<<<<< HEAD
+=======
+type collatorClient interface {
+	Account() *accounts.Account
+	ChainReader() ethereum.ChainReader
+	VMCCaller() *contracts.VMCCaller
+}
+
+>>>>>>> 6ae47790b... refactor account, and unlock account when client is created
 // SubscribeBlockHeaders checks incoming block headers and determines if
 // we are an eligible proposer for collations. Then, it finds the pending tx's
 // from the running geth node and sorts them by descending order of gas price,
@@ -17,7 +26,13 @@ import (
 func subscribeBlockHeaders(c *Client) error {
 	headerChan := make(chan *types.Header, 16)
 
+<<<<<<< HEAD
 	_, err := c.client.SubscribeNewHead(context.Background(), headerChan)
+=======
+	account := c.Account()
+
+	_, err := c.ChainReader().SubscribeNewHead(context.Background(), headerChan)
+>>>>>>> 6ae47790b... refactor account, and unlock account when client is created
 	if err != nil {
 		return fmt.Errorf("unable to subscribe to incoming headers. %v", err)
 	}
@@ -38,6 +53,7 @@ func subscribeBlockHeaders(c *Client) error {
 	}
 }
 
+<<<<<<< HEAD
 // watchShards checks if we are an eligible proposer for collation for
 // the available shards in the VMC. The function calls getEligibleProposer from
 // the VMC and proposes a collation if conditions are met
@@ -47,6 +63,14 @@ func watchShards(c *Client, head *types.Header) error {
 	if len(accounts) == 0 {
 		return fmt.Errorf("no accounts found")
 	}
+=======
+// checkShardsForProposal checks if we are an eligible proposer for
+// collation for the available shards in the VMC. The function calls
+// getEligibleProposer from the VMC and proposes a collation if
+// conditions are met
+func checkShardsForProposal(c collatorClient, head *types.Header) error {
+	account := c.Account()
+>>>>>>> 6ae47790b... refactor account, and unlock account when client is created
 
 	if err := c.unlockAccount(accounts[0]); err != nil {
 		return fmt.Errorf("cannot unlock account. %v", err)
@@ -69,8 +93,17 @@ func watchShards(c *Client, head *types.Header) error {
 			}
 		}
 
+<<<<<<< HEAD
 		s++
 	}
+=======
+// isAccountInValidatorSet checks if the client is in the validator pool because
+// we can't guarantee our tx for deposit will be in the next block header we receive.
+// The function calls IsValidatorDeposited from the VMC and returns true if
+// the client is in the validator pool
+func isAccountInValidatorSet(c collatorClient) (bool, error) {
+	account := c.Account()
+>>>>>>> 6ae47790b... refactor account, and unlock account when client is created
 
 	return nil
 }
